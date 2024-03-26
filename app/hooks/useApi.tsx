43445239a7ad142fe78c15b617dfa3fb7185cfe8
Finley Sherwood <me@itsf.in>
2024-03-26 12:41:38 +1000

    Add typings and implementations for remaining ws and http APIs

--- app/hooks/useApi.tsx
+++ app/hooks/useApi.tsx
@@ -10,18 +10,23 @@ export function useApi() {
     return res
   }
 
-  return {
-    negotiate: async (userId?: string) =>
-      await fetch(baseUrl + `/negotiate?userId=${userId ?? gameInfo.userId}`, { method: "POST" })
-        .then(ok)
-        .then(res => res.json())
-        .then((res: Api.Response.Negotiate) => res),
+  function okOrNotFound(res: Response): Response {
+    if (res.status !== 404 && (res.status < 200 || res.status >= 300)) throw new Error("Invalid status code")
+    return res
+  }
 
+  return {
     lobbyGet: (lobbyId?: string) =>
       fetch(baseUrl + `/lobbies/${lobbyId ?? gameInfo.lobbyId}`, { method: "GET" })
         .then(ok)
         .then(res => res.json())
         .then((res: Api.Response.LobbyGet) => res),
+
+    lobbyPatch: (settings: Partial<Api.GameSettings>, lobbyId?: string) =>
+      fetch(baseUrl + `/lobbies/${lobbyId ?? gameInfo.lobbyId}`, { method: "PATCH", body: JSON.stringify(settings) })
+        .then(ok)
+        .then(res => res.json())
+        .then((res: Api.Response.LobbyPatch) => res),
 
     lobbyPut: (lobbyId?: string) =>
       fetch(baseUrl + `/lobbies/${lobbyId ?? gameInfo.lobbyId}`, { method: "PUT" })
@@ -29,17 +34,57 @@ export function useApi() {
         .then(res => res.json())
         .then((res: Api.Response.LobbyPut) => res),
 
+    negotiate: async (userId?: string) =>
+      await fetch(baseUrl + `/negotiate?userId=${userId ?? gameInfo.userId}`, { method: "POST" })
+        .then(ok)
+        .then(res => res.json())
+        .then((res: Api.Response.Negotiate) => res),
+
+    userDelete: (lobbyId?: string, userId?: string) =>
+      fetch(baseUrl + `/lobbies/${lobbyId ?? gameInfo.lobbyId}/users/${userId ?? gameInfo.userId}`, {
+        method: "DELETE"
+      })
+        .then(okOrNotFound)
+        .then(() => {}),
+
+    userGet: (lobbyId?: string, userId?: string) =>
+      fetch(baseUrl + `/lobbies/${lobbyId ?? gameInfo.lobbyId}/users/${userId ?? gameInfo.userId}`, { method: "PUT" })
+        .then(ok)
+        .then(res => res.json())
+        .then((res: Api.Response.UserGet) => res),
+
     userPut: (lobbyId?: string, userId?: string) =>
       fetch(baseUrl + `/lobbies/${lobbyId ?? gameInfo.lobbyId}/users/${userId ?? gameInfo.userId}`, { method: "PUT" })
         .then(ok)
         .then(res => res.json())
         .then((res: Api.Response.UserPut) => res),
 
-    userDelete: (lobbyId?: string, userId?: string) =>
-      fetch(baseUrl + `/lobbies/${lobbyId ?? gameInfo.lobbyId}/users/${userId ?? gameInfo.userId}`, {
-        method: "DELETE "
+    voteDelete: (lobbyId?: string, userId?: string) =>
+      fetch(baseUrl + `/lobbies/${lobbyId ?? gameInfo.lobbyId}/votes/${userId ?? gameInfo.userId}`, {
+        method: "DELETE"
+      })
+        .then(okOrNotFound)
+        .then(() => {}),
+
+    voteGetAll: (lobbyId?: string) =>
+      fetch(baseUrl + `/lobbies/${lobbyId ?? gameInfo.lobbyId}/votes`, { method: "GET" })
+        .then(ok)
+        .then(res => res.json())
+        .then((res: Api.Response.VoteGetAll) => res),
+
+    voteGet: (lobbyId?: string, userId?: string) =>
+      fetch(baseUrl + `/lobbies/${lobbyId ?? gameInfo.lobbyId}/votes/${userId ?? gameInfo.userId}`, { method: "GET" })
+        .then(ok)
+        .then(res => res.json())
+        .then((res: Api.Response.VoteGet) => res),
+
+    votePut: (choice: string, lobbyId?: string, userId?: string) =>
+      fetch(baseUrl + `/lobbies/${lobbyId ?? gameInfo.lobbyId}/votes/${userId ?? gameInfo.userId}`, {
+        method: "PUT",
+        body: JSON.stringify({ choice })
       })
         .then(ok)
-        .then(() => {})
+        .then(res => res.json())
+        .then((res: Api.Response.VotePut) => res)
   }
 }
--- app/types/Api.ts
+++ app/types/Api.ts
@@ -12,6 +12,7 @@ export namespace Api {
 
   export type Lobby = {
     lobbyId: string
+    hostId: string
     userIds: string[]
     wins: Record<string, number>
     settings: Api.GameSettings
@@ -22,17 +23,40 @@ export namespace Api {
     lobbyId: string
   }
 
+  export type VoteSingle = {
+    lobbyId: string
+    userId: string
+    choice: string
+  }
+
+  export type VoteGroup = {
+    lobbyId: string
+    requiredVotes: number
+    choices: string[]
+    votes: Record<string, string[]>
+  }
+
   export namespace Response {
+    export type LobbyGet = Lobby
+
+    export type LobbyPatch = Lobby
+
+    export type LobbyPut = Lobby
+
     export type Negotiate = {
       baseUrl: string
       url: string
       accessToken: string
     }
 
-    export type LobbyGet = Lobby
-
-    export type LobbyPut = Lobby
+    export type UserGet = User
 
     export type UserPut = User
+
+    export type VoteGetAll = VoteGroup
+
+    export type VoteGet = VoteSingle
+
+    export type VotePut = VoteSingle
   }
 }
