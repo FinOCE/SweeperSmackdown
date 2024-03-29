e59c034ab97793eb30cb7cb6a6724e4f70838e7b
Finley Sherwood <me@itsf.in>
2024-03-29 16:21:14 +1000

    Create inputs for game configuration

--- app/src/pages/GameConfigure.tsx
+++ app/src/pages/GameConfigure.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react"
+import React, { ChangeEvent, useCallback, useEffect, useState } from "react"
 import { useApi } from "../hooks/useApi"
 import { useGameInfo } from "../hooks/useGameInfo"
 import { useNavigation } from "../hooks/useNavigation"
@@ -6,6 +6,8 @@ import { useWebsocket } from "../hooks/useWebsocket"
 import { Websocket } from "../types/Websocket"
 import { isEvent } from "../utils/isEvent"
 import { Api } from "../types/Api"
+
+type Difficulty = "easy" | "normal" | "hard" | "hell"
 
 export function GameConfigure() {
   const { navigate } = useNavigation()
@@ -34,6 +36,46 @@ export function GameConfigure() {
   })
 
   // Setup UI state
+  const [settings, setSettings] = useState<Api.GameSettings>({
+    mode: 0,
+    height: 16,
+    width: 16,
+    mines: 40,
+    lives: 0,
+    timeLimit: 0,
+    boardCount: 0,
+    shareBoards: false
+  })
+
+  useEffect(() => {
+    let timer = setTimeout(() => {
+      console.log("Settings changed!", settings)
+    }, 500)
+
+    return () => clearTimeout(timer)
+  }, [settings])
+
+  const changeSetting = useCallback(
+    (
+      e: ChangeEvent<HTMLInputElement>,
+      key: keyof Api.GameSettings,
+      callback: (value: ChangeEvent<HTMLInputElement>) => any
+    ) => setSettings(prev => ({ ...prev, [key]: callback(e) })),
+    []
+  )
+
+  const [difficulty, setDifficulty] = useState<Difficulty>("normal")
+
+  useEffect(() => {
+    const ratio: Record<Difficulty, number> = {
+      easy: 0.078125,
+      normal: 0.15625,
+      hard: 0.234375,
+      hell: 0.3125
+    }
+    setSettings(prev => ({ ...prev, mines: Math.floor(prev.height * prev.width * ratio[difficulty]) }))
+  }, [difficulty])
+
   const [votePending, setVotePending] = useState(false)
 
   const isReady = vote?.votes?.READY?.includes(userId!) ?? false
@@ -68,6 +110,175 @@ export function GameConfigure() {
   return (
     <div>
       <p>Welcome to lobby {lobbyId}</p>
+
+      <section id="controls">
+        <pre>
+          <code>{JSON.stringify(settings)}</code>
+        </pre>
+
+        <fieldset>
+          <legend>Mode</legend>
+
+          <div>
+            <input
+              type="radio"
+              name="mode"
+              id="mode-0"
+              value="0"
+              checked={settings.mode === 0}
+              onChange={e => changeSetting(e, "mode", Number)}
+            />
+            <label htmlFor="mode-0">Classic</label>
+          </div>
+
+          <div>
+            <p>Coming soon...</p>
+          </div>
+        </fieldset>
+
+        <br />
+
+        <fieldset>
+          <legend>Difficulty</legend>
+
+          <div>
+            <input
+              type="radio"
+              name="difficulty"
+              id="difficulty-easy"
+              value="easy"
+              checked={difficulty === "easy"}
+              onChange={e => setDifficulty(e.target.value as Difficulty)}
+            />
+            <label htmlFor="mode-easy">Easy</label>
+          </div>
+
+          <div>
+            <input
+              type="radio"
+              name="difficulty"
+              id="difficulty-normal"
+              value="normal"
+              checked={difficulty === "normal"}
+              onChange={e => setDifficulty(e.target.value as Difficulty)}
+            />
+            <label htmlFor="mode-normal">Normal</label>
+          </div>
+
+          <div>
+            <input
+              type="radio"
+              name="difficulty"
+              id="difficulty-hard"
+              value="hard"
+              checked={difficulty === "hard"}
+              onChange={e => setDifficulty(e.target.value as Difficulty)}
+            />
+            <label htmlFor="mode-hard">Hard</label>
+          </div>
+
+          <div>
+            <input
+              type="radio"
+              name="difficulty"
+              id="difficulty-hell"
+              value="hell"
+              checked={difficulty === "hell"}
+              onChange={e => setDifficulty(e.target.value as Difficulty)}
+            />
+            <label htmlFor="mode-hell">Hell</label>
+          </div>
+        </fieldset>
+
+        <br />
+
+        <div>
+          <label htmlFor="height">Height</label>
+          <input
+            type="range"
+            name="height"
+            id="height"
+            min="9"
+            max="100"
+            defaultValue={settings.height}
+            onChange={e => changeSetting(e, "height", e => Number(e.target.value))}
+          />
+          <output htmlFor="height">{settings.height}</output>
+        </div>
+
+        <div>
+          <label htmlFor="width">Width</label>
+          <input
+            type="range"
+            name="width"
+            id="width"
+            min="9"
+            max="100"
+            value={settings.width}
+            onChange={e => changeSetting(e, "width", e => Number(e.target.value))}
+          />
+          <output htmlFor="width">{settings.width}</output>
+        </div>
+
+        <div>
+          <label htmlFor="lives">Lives</label>
+          <input
+            type="range"
+            name="lives"
+            id="lives"
+            min="0"
+            max="10"
+            value={settings.lives}
+            onChange={e => changeSetting(e, "lives", e => Number(e.target.value))}
+          />
+          <output htmlFor="lives">{settings.lives !== 0 ? settings.lives : "Unlimited"}</output>
+        </div>
+
+        <div>
+          <label htmlFor="timeLimit">Time limit</label>
+          <input
+            type="range"
+            name="timeLimit"
+            id="timeLimit"
+            min="0"
+            max="600"
+            step="10"
+            value={settings.timeLimit}
+            onChange={e => changeSetting(e, "timeLimit", e => Number(e.target.value))}
+          />
+          <output htmlFor="timeLimit">{settings.timeLimit !== 0 ? settings.timeLimit : "Unlimited"}</output>
+        </div>
+
+        <div>
+          <label htmlFor="boardCount">Board per round</label>
+          <input
+            type="range"
+            name="boardCount"
+            id="boardCount"
+            min="0"
+            max="25"
+            value={settings.boardCount}
+            onChange={e => changeSetting(e, "boardCount", e => Number(e.target.value))}
+          />
+          <output htmlFor="boardCount">{settings.boardCount !== 0 ? settings.boardCount : "Unlimited"}</output>
+        </div>
+
+        <br />
+
+        <div>
+          <input
+            type="checkbox"
+            name="shareBoards"
+            id="shareBoards"
+            checked={settings.shareBoards}
+            onChange={e => changeSetting(e, "shareBoards", e => e.target.checked)}
+          />
+          <label htmlFor="shareBoards">Share boards</label>
+        </div>
+      </section>
+
+      <br />
+
       <input
         type="button"
         onClick={isReady ? voteCancel : voteStart}
