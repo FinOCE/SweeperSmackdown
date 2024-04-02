a13b440884204114e5637b989b0c7d7189ef08f5
Finley Sherwood <me@itsf.in>
2024-04-02 12:04:27 +1000

    Refactor sdk setup to clean up code

--- app/src/hooks/useEmbeddAppSdk.tsx
+++ app/src/hooks/useEmbeddAppSdk.tsx
@@ -1,74 +1,64 @@
 import React, { createContext, ReactNode, useContext, useEffect, useState } from "react"
 import { DiscordSDK, DiscordSDKMock, IDiscordSDK } from "@discord/embedded-app-sdk"
 
-const EmbeddedAppSdkContext = createContext<{
+type TEmbeddedAppSdkContext = {
   sdk: IDiscordSDK | null
   mocked: boolean
-}>({ sdk: null, mocked: false })
+}
+
+const EmbeddedAppSdkContext = createContext<TEmbeddedAppSdkContext>({ sdk: null, mocked: false })
 export const useEmbeddedAppSdk = () => useContext(EmbeddedAppSdkContext)
 
 export function EmbeddedAppSdkProvider(props: { iframeId: string | null; children: ReactNode }) {
-  const [privateSdk, setPrivateSdk] = useState<IDiscordSDK | null>(null)
-  const [mocked, setMocked] = useState(false)
-  const [publicSdk, setPublicSdk] = useState<IDiscordSDK | null>(null)
+  const mocked = props.iframeId === null
+
+  const [sdk, setSdk] = useState<IDiscordSDK | null>(null)
 
   // Create SDK
   useEffect(() => {
     if (!process.env.PUBLIC_ENV__DISCORD_CLIENT_ID) return
 
-    let sdk: IDiscordSDK
-    if (props.iframeId) {
-      sdk = new DiscordSDK(process.env.PUBLIC_ENV__DISCORD_CLIENT_ID)
-      setMocked(false)
-    } else {
-      const mock = new DiscordSDKMock(process.env.PUBLIC_ENV__DISCORD_CLIENT_ID, null, null)
+    const sdk: IDiscordSDK = mocked
+      ? getMockSdk(process.env.PUBLIC_ENV__DISCORD_CLIENT_ID)
+      : new DiscordSDK(process.env.PUBLIC_ENV__DISCORD_CLIENT_ID)
 
-      mock._updateCommandMocks({
-        async authorize() {
-          return {
-            code: ""
-          }
-        },
-        async authenticate(args) {
-          return {
-            access_token: args.access_token ?? "",
-            application: {
-              description: "",
-              id: "",
-              name: ""
-            },
-            expires: "",
-            scopes: ["identify"],
-            user: {
-              discriminator: "",
-              id: args.access_token ?? "",
-              public_flags: 0,
-              username: ""
-            }
-          }
-        }
-      })
+    sdk.ready().then(() => setSdk(sdk))
 
-      sdk = mock
-      setMocked(true)
-    }
-
-    setPrivateSdk(sdk)
-
-    return () => setPrivateSdk(null)
+    return () => setSdk(null)
   }, [process.env.PUBLIC_ENV__DISCORD_CLIENT_ID])
 
-  // Set SDK when ready
-  useEffect(() => {
-    if (!privateSdk) return
-
-    privateSdk.ready().then(() => setPublicSdk(privateSdk))
-
-    return () => setPublicSdk(null)
-  }, [privateSdk])
-
   // Render provider
-  return (
-    <EmbeddedAppSdkContext.Provider value={{ sdk: publicSdk, mocked }}>{props.children}</EmbeddedAppSdkContext.Provider>
-  )
+  return <EmbeddedAppSdkContext.Provider value={{ sdk, mocked }}>{props.children}</EmbeddedAppSdkContext.Provider>
+}
+
+function getMockSdk(clientId: string) {
+  const mock = new DiscordSDKMock(clientId, null, null)
+
+  mock._updateCommandMocks({
+    async authorize() {
+      return {
+        code: ""
+      }
+    },
+    async authenticate(args) {
+      return {
+        access_token: args.access_token ?? "",
+        application: {
+          description: "",
+          id: "",
+          name: ""
+        },
+        expires: "",
+        scopes: ["identify"],
+        user: {
+          discriminator: "",
+          id: args.access_token ?? "",
+          public_flags: 0,
+          username: ""
+        }
+      }
+    }
+  })
+
+  return mock
 }
--- app/src/hooks/useUser.tsx
+++ app/src/hooks/useUser.tsx
@@ -1,12 +1,8 @@
 import React, { createContext, ReactNode, useContext, useEffect, useState } from "react"
 import { useEmbeddedAppSdk } from "./useEmbeddAppSdk"
-import { getDiscordAvatarUrl } from "../utils/getDiscordAvatarUrl"
 import { useApi } from "./useApi"
-
-export type User = {
-  id: string
-  onDiscord: boolean
-}
+import { IDiscordSDK } from "@discord/embedded-app-sdk"
+import { User } from "../types/User"
 
 const UserContext = createContext<User | null>(null)
 export const useUser = () => useContext(UserContext)
@@ -18,31 +14,33 @@ export function UserProvider(props: { children: ReactNode }) {
   const [user, setUser] = useState<User | null>(null)
 
   useEffect(() => {
-    if (!sdk) return
-
-    sdk.commands
-      // Get access token from Discord
-      .authorize({
-        client_id: process.env.PUBLIC_ENV__DISCORD_CLIENT_ID,
-        response_type: "code",
-        scope: ["identify"]
-      })
-      .then(({ code }) => api.token(code, mocked))
-      // Exchange access token for bearer token for API
-      .then(async ({ accessToken }) => {
-        const res = await api.login(accessToken, mocked)
-        setToken(res.bearerToken)
-        return accessToken
-      })
-      // Get user info from Discord with the access token
-      .then(accessToken => sdk.commands.authenticate({ access_token: accessToken }))
-      .then(auth =>
-        setUser({
-          id: auth.user.id,
-          onDiscord: mocked
-        })
-      )
+    if (sdk) login(sdk, mocked)
   }, [sdk])
+
+  async function login(sdk: IDiscordSDK, mocked: boolean) {
+    // Get oauth code from sdk
+    const { code } = await sdk.commands.authorize({
+      client_id: process.env.PUBLIC_ENV__DISCORD_CLIENT_ID,
+      response_type: "code",
+      scope: ["identify"]
+    })
+
+    // Exchange code for access token
+    const { accessToken } = await api.token(code, mocked)
+
+    // Exchange access token for bearer token for API
+    const { bearerToken } = await api.login(accessToken, mocked)
+
+    // Get user info from sdk using access token
+    const auth = await sdk.commands.authenticate({ access_token: accessToken })
+
+    // Set user context
+    setToken(bearerToken)
+    setUser({
+      id: auth.user.id,
+      onDiscord: mocked
+    })
+  }
 
   return <UserContext.Provider value={user}>{props.children}</UserContext.Provider>
 }
