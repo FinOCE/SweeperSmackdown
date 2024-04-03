5f8dd44ea2d187a89a99d242814f0003d18deec9
Finley Sherwood <me@itsf.in>
2024-04-03 11:03:35 +1000

    Create dev preview page for UI development

--- app/src/index.tsx
+++ app/src/index.tsx
@@ -1,6 +1,5 @@
 import React from "react"
 import { createRoot } from "react-dom/client"
-import { Loading } from "./components/Loading"
 import { ApiProvider } from "./hooks/useApi"
 import { EmbeddedAppSdkProvider } from "./hooks/useEmbeddAppSdk"
 import { UserProvider } from "./hooks/useUser"
@@ -8,8 +7,12 @@ import { LobbyProvider } from "./hooks/useLobby"
 import { WebsocketProvider } from "./hooks/useWebsocket"
 import { NavigationProvider } from "./hooks/useNavigation"
 import { OriginProvider } from "./hooks/useOrigin"
+import { DevPreview } from "./pages/DevPreview"
 
 function App() {
+  // Show dev preview for developing UI features without running full app
+  if (process.env.PUBLIC_ENV__DEV_PREVIEW) return <DevPreview />
+
   // Render app
   return (
     <OriginProvider>
---
+++ app/src/pages/DevPreview.tsx
@@ -0,0 +1,5 @@
+import React from "react"
+
+export function DevPreview() {
+  return <div>Hello world</div>
+}
