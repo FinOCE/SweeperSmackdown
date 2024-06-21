f5336ddec8e1efa0a8e281707558dba3308171a5
FinOCE <me@itsf.in>
2024-06-21 13:29:17 +1000

    Create player info model to store general stats

---
+++ stats/Stats/Models/PlayerInfo.cs
@@ -0,0 +1,83 @@
+﻿namespace SweeperSmackdown.Stats.Models;
+
+using Newtonsoft.Json;
+using System;
+
+public class PlayerInfo(
+    string id,
+    int? playtime = null,
+    int[]? tilesCleared = null,
+    int? minesFlagged = null,
+    int? minesHit = null,
+    int? blindGuesses = null,
+    int[]? largestBoardCompleted = null,
+    int? largestZeroSpread = null,
+    int? largestAdjacentBombCountFound = null,
+    int? longestClearStreakWithoutMine = null,
+    int? longestClearStreakWithoutMineOrFlag = null,
+    int? fewestFlagsUsed = null,
+    int? boardsPlayed = null,
+    int? boardsCompleted = null,
+    int? boardsFailed = null,
+    int? gamesPlayed = null,
+    int? gamesWon = null,
+    DateTime? firstPlayed = null,
+    DateTime? lastPlayed = null)
+{
+    [JsonProperty("id")]
+    public string Id { get; set; } = id;
+
+    [JsonProperty("playtime")]
+    public int Playtime { get; set; } = playtime ?? 0; // Duration in seconds solving boards
+
+    [JsonProperty("tilesCleared")]
+    public int[] TilesCleared { get; set; } = tilesCleared ?? [0, 0, 0, 0, 0, 0, 0, 0, 0]; // Index = adjacent mine count
+
+    [JsonProperty("minesFlagged")]
+    public int MinesFlagged { get; set; } = minesFlagged ?? 0;
+
+    [JsonProperty("minesHit")]
+    public int MinesHit { get; set; } = minesHit ?? 0;
+
+    [JsonProperty("blindGuesses")]
+    public int BlindGuesses { get; set; } = blindGuesses ?? 0;
+
+    [JsonProperty("largestBoardCompleted", NullValueHandling = NullValueHandling.Ignore)]
+    public int[]? LargestBoardCompleted { get; set; } = largestBoardCompleted; // [width, height], calculated based on volume
+
+    [JsonProperty("largestZeroSpread", NullValueHandling = NullValueHandling.Ignore)]
+    public int? LargestZeroSpread { get; set; } = largestZeroSpread;
+
+    [JsonProperty("largestAdjacentBombCountFound", NullValueHandling = NullValueHandling.Ignore)]
+    public int? LargestAdjacentBombCountFound { get; set; } = largestAdjacentBombCountFound;
+
+    [JsonProperty("longestClearStreakWithoutMine", NullValueHandling = NullValueHandling.Ignore)]
+    public int? LongestClearStreakWithoutMine { get; set; } = longestClearStreakWithoutMine;
+
+    [JsonProperty("longestClearStreakWithoutMineOrFlag", NullValueHandling = NullValueHandling.Ignore)]
+    public int? LongestClearStreakWithoutMineOrFlag { get; set; } = longestClearStreakWithoutMineOrFlag;
+
+    [JsonProperty("fewestFlagsUsed", NullValueHandling = NullValueHandling.Ignore)]
+    public int? FewestFlagsUsed { get; set; } = fewestFlagsUsed;
+
+    [JsonProperty("boardsPlayed")]
+    public int BoardsPlayed { get; set; } = boardsPlayed ?? 0;
+
+    [JsonProperty("boardsCompleted")]
+    public int BoardsCompleted { get; set; } = boardsCompleted ?? 0;
+
+    [JsonProperty("boardsFailed")]
+    public int BoardsFailed { get; set; } = boardsFailed ?? 0;
+
+    [JsonProperty("gamesPlayed")]
+    public int GamesPlayed { get; set; } = gamesPlayed ?? 0;
+
+    [JsonProperty("gamesWon")]
+    public int GamesWon { get; set; } = gamesWon ?? 0;
+
+    [JsonProperty("firstPlayed", NullValueHandling = NullValueHandling.Ignore)]
+    public DateTime? FirstPlayed { get; set; } = firstPlayed;
+
+    [JsonProperty("lastPlayed", NullValueHandling = NullValueHandling.Ignore)]
+    public DateTime? LastPlayed { get; set; } = lastPlayed;
+}
