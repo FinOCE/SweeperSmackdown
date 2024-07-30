07830b7f9daad78144a14198ad2e2ebeacf692fc
FinOCE <me@itsf.in>
2024-07-31 00:31:49 +1000

    Fix azurite and tunnel local setup

--- scripts/Run-Local.ps1
+++ scripts/Run-Local.ps1
@@ -2,40 +2,48 @@
 .SYNOPSIS
   Starts all services to run the entire service locally.
 .DESCRIPTION
-  This script starts all services to run the entire service locally. All startup
-  scripts are called to open new terminal tabs. Before this script can be used,
-  the local environment must be appropriately configured with secrets files and
-  necessary tools installed.
+  This script starts all services to run the entire service locally. All startup scripts are called to open new
+  terminal tabs. Before this script can be used, the local environment must be appropriately configured with secrets
+  files and necessary tools installed.
 #>
 
 Function Open-TerminalTab {
   Param(
     # The path to where the terminal tab should be opened.
     [Parameter(Mandatory=$false)]
-    [string] $Path = '.',
+    [string] $Path = ".",
 
     # The command to run in the terminal tab.
     [Parameter(Mandatory=$true)]
     [string] $Command
   )
 
-  wt -w 0 -d $Path -p "Windows PowerShell" cmd /k $Command
+  $ResolvedPath = Join-Path -Path $PSScriptRoot -ChildPath $Path -Resolve
+  wt -w 0 -d $ResolvedPath -p "Windows PowerShell" cmd /k $Command
 }
 
-Function Start-Tunnel {
+Function Start-Azurite {
   Param(
-    # The path to the tunnel executable and configuration.
+    # The path to run Azurite and therefore store data.
     [Parameter(Mandatory=$true)]
     [string] $Path
   )
 
-  Open-TerminalTab -Path $Path -Command "cloudflared tunnel run"
-  Write-Host "Started tunnel at $Path"
+  Open-TerminalTab -Path $Path -Command "azurite -s -l"
 }
 
-Function Start-Azurite {
-  Open-TerminalTab -Command "azurite -s -l"
-  Write-Host "Started azurite"
+Function Start-Tunnel {
+  Param(
+    # The path to run the tunnel from..
+    [Parameter(Mandatory=$true)]
+    [string] $Path,
+
+    # The ID of the tunnel to connect to.
+    [Parameter(Mandatory=$true)]
+    [string] $TunnelId
+  )
+
+  Open-TerminalTab -Path $Path -Command "cloudflared tunnel run $TunnelId"
 }
 
 Function Start-NodejsApp {
@@ -46,7 +54,6 @@ Function Start-NodejsApp {
   )
 
   Open-TerminalTab -Path $Path -Command "npm start"
-  Write-Host "Started node.js app at $Path"
 }
 
 Function Start-FunctionApp {
@@ -57,11 +64,10 @@ Function Start-FunctionApp {
   )
 
   Open-TerminalTab -Path $Path -Command "func start"
-  Write-Host "Started function app at $Path"
 }
 
-Start-Tunnel -Path '..\.cloudflared'
-Start-Azurite
-Start-FunctionApp -Path '..\api\SweeperSmackdown'
-Start-FunctionApp -Path '..\bot\Bot'
-Start-NodejsApp -Path '..\app'
+Start-Azurite -Path "..\.azurite"
+Start-Tunnel -Path "..\.cloudflared" -TunnelId "67534728-9f06-4528-ae86-7f93a2286929"
+Start-FunctionApp -Path "..\api\SweeperSmackdown"
+Start-FunctionApp -Path "..\bot\Bot"
+Start-NodejsApp -Path "..\app"
