af5d0e86bf92e1ef44aacb96f70e9013972f8eed
FinOCE <me@itsf.in>
2024-08-01 14:32:45 +1000

    Streamline option validation and pattern matching

--- bot/Bot/Commands/PlayCommand.fs
+++ bot/Bot/Commands/PlayCommand.fs
@@ -3,49 +3,55 @@
 open SweeperSmackdown.Bot.Types
 open SweeperSmackdown.Bot.Requests
 open SweeperSmackdown.Bot.Services
-open System.Threading.Tasks
+
+type PlayCommandValidResult = { channelId: string }
 
 type PlayCommand (configurationService: IConfigurationService) =
-    let TaskError message = Task.FromResult <| Error message
+    let validate (interaction: Interaction) =
+        let (|Channel|_|) (name: string) (options: CommandInteractionDataOption list) =
+            options
+                |> Seq.map(fun o ->
+                    match o with
+                    | { Value = Some(CommandInteractionDataOptionValue.String channelId) }
+                        when o.Type = ApplicationCommandOptionType.CHANNEL
+                        && o.Name = name -> Some(channelId)
+                    | _ -> None
+                )
+                |> Seq.filter(fun o -> o.IsSome)
+                |> Seq.map(fun o -> o.Value)
+                |> Seq.tryHead
 
-    let (|Valid|NoChannelOption|NoOptions|MissingData|) (interaction: Interaction) =
-        let (|Channel|_|) (options: CommandInteractionDataOption list) =
-            match options |> Seq.tryFind(fun o -> o.Type = ApplicationCommandOptionType.CHANNEL) with
-            | Some { Value = Some(CommandInteractionDataOptionValue.String channelId) } -> Some (Channel channelId)
-            | _ -> None
+        // TODO: Clean up `Seq` usage (I'm sure there's a cleaner way to do it)
+        // TODO: Create versions of above for all option types and move to shared file
 
         match interaction with
-        | { Data = Some { Options = Some (Channel channelId) } } -> Valid channelId
-        | { Data = Some { Options = Some _ } } -> NoChannelOption
-        | { Data = Some { Options = None } } -> NoOptions
-        | { Data = None } -> MissingData
+        | { Data = Some { Options = Some (Channel "channel" channelId) } } -> Ok { channelId = channelId }
+        | { Data = Some { Options = Some _ } } -> Error "No channel option in interaction data"
+        | { Data = Some { Options = None } } -> Error "No options in interaction data"
+        | { Data = None } -> Error "Missing interaction data"
 
-    let Run (interaction: Interaction) =
+    let run (interaction: Interaction) = task {
         match configurationService.TryGetValue "DISCORD_APPLICATION_ID" with
-        | None -> TaskError "Application missing discord application ID"
+        | None -> return Error "Application missing discord application ID"
         | Some applicationId ->
-            match interaction with
-            | MissingData -> TaskError "Missing interaction data"
-            | NoOptions -> TaskError "No options in interaction data"
-            | NoChannelOption -> TaskError "No channel option in interaction data"
-            | Valid channelId ->
-                task {
-                    let! invite = (CreateChannelInvite
-                        .Build(
-                            maxAge = 0,
-                            targetType = InviteTargetType.EMBEDDED_APPLICATION,
-                            targetApplicationId = applicationId
-                        )
-                        .SendAsync(channelId))
+            match validate interaction with
+            | Error message -> return Error message
+            | Ok ({ channelId = channelId }) ->
+                let! invite = (CreateChannelInvite
+                    .Build(
+                        maxAge = 0,
+                        targetType = InviteTargetType.EMBEDDED_APPLICATION,
+                        targetApplicationId = applicationId
+                    )
+                    .SendAsync(channelId))
 
-                    return Ok (InteractionCallback.Build(
-                        InteractionCallbackType.CHANNEL_MESSAGE_WITH_SOURCE,
-                        InteractionCallbackMessageData.Build(
-                            Content = $"https://discord.gg/{invite.Code}",
-                            AllowedMentions = AllowedMentions.Build(
-                                Parse = []
-                            )
+                return Ok (InteractionCallback.Build(
+                    InteractionCallbackType.CHANNEL_MESSAGE_WITH_SOURCE,
+                    InteractionCallbackMessageData.Build(
+                        Content = $"https://discord.gg/{invite.Code}",
+                        AllowedMentions = AllowedMentions.Build(
+                            Parse = []
                         )
-                    ))
-                }
-                
+                    )
+                ))
+    }
